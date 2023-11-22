1d03c9fa3b3f0811261a49f8ca1cc48987c80029
William Mori <wmori@ucsb.edu>
2023-11-22 00:39:22 -0800

    use gitignore

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./creds.json');
+const serviceAccount = require('./creds.js');
 
 initializeApp({
     credential: cert(serviceAccount)
