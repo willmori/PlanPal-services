d5bfad8676a581953842af8c89a8a6f9f90031dd
William Mori <wmori@ucsb.edu>
2023-11-23 00:32:26 -0800

    adjust spacing

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -3,21 +3,22 @@ const { db } = require('../firebase');
 class EventsController {
     
     static async getEvents(req, res) {
-        try {
-            const collectionRef = db.collection('events');
-            const snapshot = await collectionRef.get();
+      try {
+        const collectionRef = db.collection('events');
+        const snapshot = await collectionRef.get();
         
-            let documents = [];
-            snapshot.forEach(doc => {
-              documents.push({ id: doc.id, ...doc.data() });
-            });
+        let documents = [];
+        snapshot.forEach(doc => {
+          documents.push({ id: doc.id, ...doc.data() });
+        });
         
-            res.status(200).send(documents);
-          } catch (error) {
-            console.error("Error getting documents", error);
-            res.status(500).send(error);
-          }
+        res.status(200).send(documents);
+      } catch (error) {
+        console.error("Error getting documents", error);
+        res.status(500).send(error);
+      }
     }
+    
 }
 
 module.exports = EventsController;
